4718ec496047c826e96b18f5cb8dcf2e36d92581
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-02-09 15:41:45 -0500

    project1 done

--- app/models/course.rb
+++ app/models/course.rb
@@ -18,4 +18,21 @@ class Course < ApplicationRecord
   has_many :enrolled_students, 
     through: :enrollments, 
     source: :student
+
+  belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true
+  
+  has_many :prerequisites,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+  
 end
